df4ed7b72bbd28371e217472c325033e498ed81d
husaininajib <husaininajib98@gmail.com>
2021-12-13 20:03:33 +0800

    added projects section

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector(".hamburger")
 const navPanel = document.querySelector(".nav-panel")
+const navLink = document.querySelectorAll(".nav-link")
 
 hamburger.addEventListener("click", () => {
     navPanel.classList.toggle("show")
     hamburger.classList.toggle("rotate")
+})
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navPanel.classList.remove("show")
+    hamburger.classList.remove("rotate")
+
+    })
 })
