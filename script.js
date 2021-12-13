f7748b5806431bf535edfc2d4dd9dc2e2479e1ff
husaininajib <husaininajib98@gmail.com>
2021-12-14 07:32:59 +0800

    added media queries for lower screen size

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector(".hamburger")
 const navPanel = document.querySelector(".nav-panel")
 const navLink = document.querySelectorAll(".nav-link")
+const arrowToTop = document.querySelector(".arrow-to-home")
+const header = document.querySelector("header")
 
 hamburger.addEventListener("click", () => {
     navPanel.classList.toggle("show")
@@ -12,4 +14,14 @@ navLink.forEach(link => {
     hamburger.classList.remove("rotate")
 
     })
+})
+
+window.addEventListener("scroll", () => {
+    const headerHeight = header.clientHeight
+    const scrollDistance = window.pageYOffset
+    if (scrollDistance > headerHeight / 2) {
+        arrowToTop.classList.add("appear")
+    } else {
+        arrowToTop.classList.remove("appear")
+    }
 })
